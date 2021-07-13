1023cc6b1e37163a4e5bc30b6d3cfdb7e2f7d2d9
Ankitchouhan9993 <78790534+Ankitchouhan9993@users.noreply.github.com>
2021-07-13 22:10:57 +0530

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,11 @@
+This is a website which will show time remain to get new year....
+we have used .... Date object using java script ..
+some css styling .
+it is responsive also for table and mobile and desktop view.
+we have used Dom manipulation .etc...
+
+
+
+
+built by Ankit Chouhan ... in 1 hour ...
+thankyou.
---
+++ script.js
@@ -0,0 +1,19 @@
+function Callme(){
+    const newyear= new Date("1 jan 2022");
+   const currenttime=new Date();
+   const differ=newyear-currenttime;
+   const days= Math.floor(differ/(1000*60*60*24));
+   const hours=Math.floor(differ/1000/60/60-days*24);
+   const minutes=Math.floor((differ/1000/60-(days*24*60))-hours*60);
+   const seconds=Math.floor(differ/1000-(days*24*60*60)-hours*60*60-minutes*60);
+   updateData(days,hours,minutes,seconds);
+}
+setInterval(()=>{
+    Callme();
+}, 1000);
+function updateData(days,hours,minutes,seconds){
+    document.querySelector('.days').children[0].innerHTML=days;
+    document.querySelector('.hours').children[0].innerHTML=hours;
+    document.querySelector('.minutes').children[0].innerHTML=minutes+"";
+    document.querySelector(".seconds").children[0].innerHTML=seconds+"";
+}
